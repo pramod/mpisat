f43e7120ac631738b6486e370eb735761b022338
Pramod Subramanyan <pramod.subramanyan@gmail.com>
2013-01-14 22:07:22 -0500

    This idea of looking beyond the top of the heap to find the next branch literal.

--- psat/core/run.sh
+++ psat/core/run.sh
@@ -6,13 +6,9 @@ for i in `cat benchmarks/sr2008/good.list`
 do
     echo "running $i [count=$cnt]"
     let "cnt=cnt+1"
-    for n in 4 8 12;
+    for n in 2 4 8;
     do
-        for j in 16 256 1024;
-        do
-            mpirun -np $n ./minisat -cpu-lim=1200 -share-act-vars=$j -freq-th=2 benchmarks/sr2008/$i >& benchmarks/sr2008/$i.psatv3.n$n.sav.$j.f2
-            mpirun -np $n ./minisat -cpu-lim=1200 -share-act-vars=$j -freq-th=3 benchmarks/sr2008/$i >& benchmarks/sr2008/$i.psatv3.n$n.sav.$j.f3
-            mpirun -np $n ./minisat -cpu-lim=1200 -share-act-vars=$j -freq-th=4 benchmarks/sr2008/$i >& benchmarks/sr2008/$i.psatv3.n$n.sav.$j.f4
-        done
+            mpirun -np $n ./minisat -cpu-lim=1200 -heap-depth=$n benchmarks/sr2008/$i >& benchmarks/sr2008/$i.psatv4.n$n.noflip
+            mpirun -np $n ./minisat -cpu-lim=1200 -heap-depth=$n -flip-sign benchmarks/sr2008/$i >& benchmarks/sr2008/$i.psatv4.n$n.flip
     done
 done
