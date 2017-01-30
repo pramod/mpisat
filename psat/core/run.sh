feef09c5976605067be0f0df570e24260f0e3cff
Pramod Subramanyan <pramod.subramanyan@gmail.com>
2017-01-30 15:15:10 -0500

    adding minisat results.

--- psat/core/run.sh
+++ psat/core/run.sh
@@ -6,9 +6,9 @@ for i in `cat benchmarks/sr2008/good.list`
 do
     echo "running $i [count=$cnt]"
     let "cnt=cnt+1"
-    for n in 2 4 8;
+    for n in 4 8 12;
     do
-            mpirun -np $n ./minisat -cpu-lim=1200 -heap-depth=$n benchmarks/sr2008/$i >& benchmarks/sr2008/$i.psatv4.n$n.noflip
-            mpirun -np $n ./minisat -cpu-lim=1200 -heap-depth=$n -flip-sign benchmarks/sr2008/$i >& benchmarks/sr2008/$i.psatv4.n$n.flip
+            mpirun -np $n ./minisat -cpu-lim=1200 -heap-depth=$n benchmarks/sr2008/$i >& benchmarks/sr2008/no_iprobe/$i.psatv4.n$.no_iprobe
+            # mpirun -np $n ./minisat -cpu-lim=1200 -heap-depth=$n -flip-sign benchmarks/sr2008/$i >& benchmarks/sr2008/$i.psatv4.n$n.flip
     done
 done
